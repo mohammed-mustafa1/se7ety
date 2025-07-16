b956e7e944ffbda2773ae627febd2a1ea66b0daa
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2025-07-17 01:44:48 +0300

    change app icon and rename app

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -2,4 +2,5 @@ class AppAssets {
   static const String loadingAnimation = 'assets/animations/heart1.json';
   static const String loadingAnimation2 = 'assets/animations/heart2.json';
   static const String loadingAnimation3 = 'assets/animations/heart3.json';
+  static const String logo = 'assets/images/logo.png';
 }
--- lib/core/routers/app_routers.dart
+++ lib/core/routers/app_routers.dart
@@ -1,11 +1,12 @@
 import 'package:go_router/go_router.dart';
+import 'package:se7ety/features/intro/page/splash_screen.dart';
 
 class AppRouter {
   static const String splash = '/';
 
   static final routers = GoRouter(
     routes: [
-      // GoRoute(path: splash, builder: (context, state) => SplashScreen()),
+      GoRoute(path: splash, builder: (context, state) => SplashScreen()),
     ],
   );
 }
